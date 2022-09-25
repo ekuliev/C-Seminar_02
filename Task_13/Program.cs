49e185b3638ce723a8c05a222e82f6e2459efff1
Elmar_Kouliev <ker1912@mail.ru>
2022-09-25 22:48:08 +0300

    Задача 13

---
+++ Task_13/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 13: выводит третью цифру заданного числа или сообщает, что третьей цифры нет
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+int Digit = 0;
+int ExtractDigit(int number)
+{
+    while (number >= 100)
+    {
+        Digit = number% 10;
+        number = number/10;
+    }
+    return Digit;
+}
+if (number < 100)
+{
+    Console.WriteLine("3-й цифры нет");
+}
+else
+{
+ExtractDigit(number);
+Console.WriteLine(Digit);
+}
+    
