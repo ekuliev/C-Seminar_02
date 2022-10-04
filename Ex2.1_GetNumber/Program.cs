3835a627c71c7a64d92af2b8b1760da70b947d68
Elmar_Kouliev <ker1912@mail.ru>
2022-10-04 21:57:51 +0300

    Dir name

---
+++ Ex2.1_GetNumber/Program.cs
@@ -0,0 +1,27 @@
+﻿// 
+int Getnumber()
+{
+    Console.WriteLine("Insert number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+}
+
+bool CheckMod(int a, int mod)
+{
+    return (a % mod == 0);
+}
+
+int a = Getnumber();
+if (CheckMod(int.MaxValue a)
+//Console.WriteLine("Insert number: ");
+//int a = Convert.ToInt32(Console.ReadLine());
+
+
+bool result;
+bool del7 = (a %7 == 0);
+bool del23 = (a %23 == 0);
+result = (del7 && del23);
+
+if (result)
+{
+    Console.WriteLine("Ridht!");
+}
